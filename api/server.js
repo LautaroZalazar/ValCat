3d1a25c338b206eec48813bafab13d96338d88ca
LautaroZalazar <lautyzalazar10@gmail.com>
2022-12-22 19:53:19 -0300

    [Feat] Config added to server

--- api/server.js
+++ api/server.js
@@ -5,6 +5,8 @@ dotenv.config()
 const { PORT } = process.env
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 const server = app.listen(PORT, () => {
     console.log(`Server listening at port: ${PORT}`);
