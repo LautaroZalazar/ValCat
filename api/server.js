9ec953a22311921cf96bbb8b0a4bdcab0ce583ab
LautaroZalazar <lautyzalazar10@gmail.com>
2022-12-22 22:09:18 -0300

    [Feat] Database added

--- api/server.js
+++ api/server.js
@@ -1,12 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from "mongoose";
 dotenv.config()
 
-const { PORT } = process.env
+const { PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+mongoose.set('strictQuery', false)
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_NAME}.f17rva1.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {
+    console.log("Database connected");
+})
+.catch((err) =>{
+    console.log("Error in database conection", err);
+})
 
 const server = app.listen(PORT, () => {
     console.log(`Server listening at port: ${PORT}`);
