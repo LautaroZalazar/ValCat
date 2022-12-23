378f5f66ef6db9e04e8de3f81e8aaf9d2b6e2633
LautaroZalazar <lautyzalazar10@gmail.com>
2022-12-23 17:16:50 -0300

    [Feat] getAllCakes

--- api/controllers/cake/GET/getAllCakes.js
+++ api/controllers/cake/GET/getAllCakes.js
@@ -6,14 +6,20 @@ export const getAllCakes = async (req, res) => {
     if (name) {
       const cakes = await Cake.find({
         name: new RegExp(".*" + name + ".*", "i"),
-      });
+      })
+        .populate({ path: "model" })
+        .populate({ path: "filling" })
+        .populate({ path: "topping" });
       if (cakes[0]) {
         res.json(cakes);
       } else {
         res.status(404).send("Cakes not found");
       }
     } else {
-      const cakes = await Cake.find();
+      const cakes = await Cake.find()
+        .populate({ path: "model" })
+        .populate({ path: "filling" })
+        .populate({ path: "topping" });
       if (cakes[0]) {
         res.json(cakes);
       } else {
